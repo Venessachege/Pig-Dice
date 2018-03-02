265e79401bca4430028d59bb75a63d0dbfcf4dff
venessachege <vchege.203@gmail.com>
2018-03-02 14:52:34 +0300

    Create the basic structure

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+var player=function(name,score){
+  this.name=name;
+  this.score=0;
+}
+var diceRoller=function(){
+  size=6;
+  // return="";
+  for(var i=1;i<=size;i++){
+  return (Math.floor(Math.random()*7));
+  }
+}
+diceRoller();
+$(document).ready(function(){
+   $("form#form").submit(function(event){
+     event.preventDefault();
+     var name=$("input#player1").val();
+     $("#titles").append(name);
+     $("form#form").hide();
+   });
+   $("form#form2").submit(function(event){
+     event.preventDefault();
+     var name=$("input#player2").val();
+     $("#title").append(name);
+     $("form#form2").hide();
+});
+   $("#roll").submit(function(){
+    
+    console.log(diceRoller());
+
+   });
+
+});
